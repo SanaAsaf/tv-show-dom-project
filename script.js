b5f0eac253744f8254ad9365d2cec4d0d201c2bd
SanaAsaf <sanaasaf@gmail.com>
2023-02-17 20:28:35 +0000

    Level 400
    Level 400 back button still not working

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-const url ="https://api.tvmaze.com/shows/82/episodes";
+const url = getUrlFromId(82);
 let allEpisodes =[];
+const searchInput = document.getElementById("searchText");
+const dropDown = document.getElementById("mySelect");
+const backButton = document.getElementById("backButton");
+const dropDownShow = document.getElementById("selectShow");
 
-//You can edit ALL of the code here
+
 function setup() {
+  makeShowDropdownItems();
   //fetch json data
   fetch(url).then(res => res.json()).then(data => {
     allEpisodes = data;
     makePageForEpisodes(allEpisodes);
   })
   .catch((err) => console.error(err));
- //let backButton = document.getElementById("backButton");
-  backButton.disabled = "true";
+  backButton.disabled = true;
 
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  const dropDown = document.getElementById("mySelect");
-  const backButton = document.getElementById("backButton");
-  // clear out 
+    
+  // clear out the search input box and the drop down
   rootElem.innerHTML="";
+  dropDown.innerHTML="";
+  backButton.disabled=true;
 
   const countEpisodes = document.createElement("p");
   if (episodeList.length>72){
   countEpisodes.innerText = `Showing all 73 episodes`;
-  backButton.disabled = "true";
+  backButton.disabled = true ;
   }
   else {
   countEpisodes.innerText = `Showing ${episodeList.length}/73`;  
-  backButton.disabled = "false";
+  backButton.disabled = false;
+  backButton.addEventListener("click",(event)=>{ 
+    //dropDown.innerHTML="";
+    makePageForEpisodes(allEpisodes);
+  });
   }
   rootElem.appendChild(countEpisodes);
 
@@ -44,7 +53,7 @@ function makePageForEpisodes(episodeList) {
   image.src = episode.image.medium;
   rootElem.appendChild(image);
 
-  // add summar paragraph and the episode summary is actually HTML
+  // add summary paragraph and the episode summary is actually HTML
   const summaryparagraph = document.createElement("p");
   summaryparagraph.innerHTML =`${episode.summary}`
   rootElem.appendChild(summaryparagraph);
@@ -53,17 +62,19 @@ function makePageForEpisodes(episodeList) {
   
   const option = document.createElement("option")
   option.text = `S${episode.season.toString().padStart(2,"0")}E${episode.number.toString().padStart(2,"0")}:${episode.name}`;
-  //dropDown.add(option);
   option.value = episode.id;
   dropDown.appendChild(option);
 
   });
 
 }
+/**
+ * Event Listeners
+ */
 
-// Search an episode and display the episodes searched
-const searchInput = document.getElementById("searchText");
 
+
+// Search input an episode and display the episodes searched
 searchInput.addEventListener("input",(event)=>{
   const searchString = event.target.value.toLowerCase();
   const filteredEpisodes = allEpisodes.filter((episode)=>{
@@ -72,9 +83,8 @@ searchInput.addEventListener("input",(event)=>{
   makePageForEpisodes(filteredEpisodes);
 });
 
-const selectInput = document.getElementById("mySelect");
-
-selectInput.addEventListener("change",(event)=>{
+// Select from drop down an episode and display all the episode 
+dropDown.addEventListener("change",(event)=>{
   // we now have episode.target.value which is the id of the episode selected in the drop down
   const idSelectedByUser = Number(event.target.value);
   const selectedEpisode = allEpisodes.find(
@@ -84,4 +94,31 @@ selectInput.addEventListener("change",(event)=>{
   makePageForEpisodes([selectedEpisode]);
   }
 });
+
+// Select from drop down a show and display all the shows
+selectShow.addEventListener("change",(event)=>{
+  const showIdSelectedByUser = Number(event.target.value);
+  const nextFetchUrl = getUrlFromId(showIdSelectedByUser);
+});
+
+
+
+
 window.onload = setup;
+
+/***
+ * Helper functions
+ */
+
+function makeShowDropdownItems() {
+  const allShows = getAllShows();
+  allShows.forEach((show) => {
+    const option = document.createElement("option");
+    option.textContent = show.name;
+    option.value = show.id;
+    selectShow.appendChild(option);
+  });
+}
+
+function getUrlFromId(id) {
+  return `https://api.tvmaze.com/shows/${id}/episodes`;}
